1dba01d144637fb66bbe0e2889703772f65ae712
Se-an Wong Chung <sean@webonweb.nl>
2025-06-04 15:54:33 +0200

    Add Travelpayouts verification script

--- app/app.vue
+++ app/app.vue
@@ -3,3 +3,24 @@
     <NuxtPage />
   </UApp>
 </template>
+
+<script setup>
+useHead({
+  script: [
+    {
+      innerHTML: `(function () {
+        var script = document.createElement("script");
+        script.async = 1;
+        script.src = 'https://emrld.ltd/NDIzMjE3.js?t=423217';
+        document.head.appendChild(script);
+      })();`,
+      tagPosition: 'head',
+      type: 'text/javascript',
+      'data-noptimize': '1',
+      'data-cfasync': 'false',
+      'data-wpfc-render': 'false'
+    }
+  ],
+  __dangerouslyDisableSanitizers: ['script'] // if needed
+});
+</script>
